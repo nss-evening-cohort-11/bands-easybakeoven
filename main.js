b6e20ba7157dae9079da102c6ece5934f4e2feee
Dylan Trevor Rowe <Dylanthk@gmail.com>
2020-02-15 11:19:18 -0600

    Dylan push

--- main.js
+++ main.js
@@ -1,67 +1,85 @@
 const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-  };
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+const merchArray = [
+  {
+    Merch: "shirt",
+    description: "Easy Bake bong shirt",
+    price: "$6.66",
+    imageUrl:
+      "https://i.etsystatic.com/17353358/r/il/22302b/1991411771/il_794xN.1991411771_h7qn.jpg",
+    id: 1
+  },
+  {
+    Merch: "Bong",
+    description: "super rad bong",
+    price: "$25",
+    imageUrl: "https://media.giphy.com/media/wsZeooqD2EqRw8AhJf/giphy.gif",
+    id: 2
+  },
+  {
+    Merch: "hoodie",
+    description: "cool weed hoodie",
+    price: "free thirty",
+    imageUrl:
+      "https://ae01.alicdn.com/kf/H0119555b8f4b4ab5adf545449a28d936t/Nuns-And-Bongs-Weed-Funny-Smoking-Men-Women-Unisex-1328-Hiphop-Summer-Hoodies-Sweatshirts.jpg_q50.jpg",
+    id: 3
+  },
+  {
+    Merch: "Beanie",
+    description: "pot leaf beanie",
+    price: "$your soul!!!",
+    imageUrl:
+      "https://lh3.googleusercontent.com/proxy/_jTiBmT7TXA_WwxFTZxck-SByl_Vh4WaXdAvrd9GoneJ3bj8JisPXxJGQkZOvOYd7ReM_iw6E2tVyOkSDuczLi09J_hF6cyyqAgvRdFQ-nVdEWf8ALbPDUTUHwp-VnFCgp2y8RcFyY4",
+    id: 4
+  },
+  {
+    Merch: "dirty sweatpants",
+    description: "plain ole sweatpants",
+    price: "$5.00",
+    imageUrl:
+      "https://lh3.googleusercontent.com/proxy/Do3guNocPuktZaslUtFeI601lXJm8QX8OCeMI86_8DXG9wT23eWXlEkexqmOR-mvIGCKT5tA27I-XNpMtAvHwBJcM06nMXMCA-wmPLjbKd-TFLMiRqJSnRhNlA",
+    id: 5
+  },
+  {
+    Merch: "Beer Stein",
+    description: "Easy Bake beer Stein",
+    price: "$6.00",
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/412BCQvrvHL.jpg",
+    id: 6
+  }
+];
+const merchBuilder = productArray => {
+  let domString = "";
+  for (let i = 0; i < productArray.length; i++) {
+    domString += `
+    <div class="card">
+    <img class="card-img-top merchPics" src=${productArray[i].imageUrl} alt="Card image cap">
+    <div class="card-body">
+    <h5 class="card-title">${productArray[i].Merch}</h5>
+    <p class="card-text">${productArray[i].description}</p>
+    <p class="card-text">${productArray[i].price}</p>
+    <button type="button" onclick="buttonFunction (${merchArray[i].id})" class="btn btn-primary">purchase</button>
   
-  const merchArray = [
-    {
-      Merch: "shirt",
-      description: "Easy Bake bong shirt",
-      price: 6.66
-    },
-  
-    {
-      Merch: "Bong",
-      description: "super rad bong ",
-      price: 25.0
-    },
-    {
-      Merch: "hoodie",
-      description: "cool black metal weed hoodie",
-      price: "free thirty"
-    },
-    {
-      Merch: "Beanie",
-      description: "pot leaf beanie",
-      price: "your soul"
-    },
-    {
-      Merch: "sweatpants",
-      description: "plain ole sweatpants",
-      price: 5.0
-    },
-    {
-      Merch: "Beer Stein",
-      description: "Easy Bake beer Stein",
-      price: 6.0
+    
+  </div> 
+  </div>`;
+  }
+  printToDom("cardPrint", domString);
+};
+
+buttonFunction = id => {
+  for (let i = 0; i < merchArray.length; i++) {
+    if (merchArray[i].id === id) {
+      alert("Purchase successfull!");
     }
-  ];
-  
-  merchBuilder = poopArray => {
-    let domString = "";
-    for (let i = 0; i < merchArray.length; i++) {
-      domString += `<div class="card mb-3" id= "merchCard" style="max-width: 540px;"></div>
-      <div class="row no-gutters">
-      <div class="col-md-4">
-      <img src="..." class="card-img" alt="...">
-      </div>
-      <div class="col-md-8">
-      <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      <button type="button" class="btn btn-primary">Primary</button>
-      </div>
-      </div>
-      </div>
-      </div>`;
-    }
-    printToDom("cardPrint", domString);
-  };
-  
-  const init = () => {
-    merchBuilder(merchArray);
-  };
-  
-  init();
-  
+  }
+};
+buttonFunction();
+
+init = () => {};
+merchBuilder(merchArray);
+
+init();
